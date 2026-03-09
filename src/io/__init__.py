33a0f202c52aa2a933a2feb1779f73d5c54a76d2
mrcyme <simeonmcl@gmail.com>
2026-03-09 17:21:51 +0100

    Refactor __all__ in I/O module to an empty list for clarity

--- src/io/__init__.py
+++ src/io/__init__.py
@@ -1,4 +1,3 @@
-from src.io.gpkg_reader import normalize_semantic_class, read_surfaces
-from src.io.tiles_writer import write_3d_tiles
+"""I/O modules for GeoPackage inputs and 3D Tiles outputs."""
 
-__all__ = ["normalize_semantic_class", "read_surfaces", "write_3d_tiles"]
+__all__: list[str] = []
